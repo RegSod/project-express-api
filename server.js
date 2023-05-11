858a76a12e4e7bb7051e7afe67e517a891a9dccc
Regina Söderberg <reginawise@gmail.com>
2023-05-11 17:54:51 +0200

    added text

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import antiqueObjects from "./data/antiqe-objects.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -24,6 +25,52 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+app.get("/antiques", (req, res) => {
+  const antiques = antiqueObjects;
+  if (antiques) { 
+ res.status(200).json({
+   success: true,
+   message: "OK",
+   body: {
+    antiqueObjects: antiques
+ } 
+});
+}
+  else 
+{ 
+  res.status(500).json({
+    success: false,
+    message: "I'm sorry, something went wrong",
+    body: {}
+ 
+  }); 
+}
+});
+
+app.get("/antiques/:id", (req, res) => {
+  const singleAntiqe = antiqueObjects.find((antiqe) => {
+    const { id } = req.params;
+    return antiqe._id === Number(id);
+  });
+  if (singleAntiqe) { 
+ res.status(200).json({
+   success: true,
+   message: "OK",
+   body: {
+    antique: singleAntiqe
+ } 
+});
+}
+  else 
+{ 
+  res.status(404).json({
+    success: false,
+    message: "I'm sorry, the item you are looking for is not found here",
+    body: {}
+ 
+  }); 
+}
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
